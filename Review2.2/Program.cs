ae4f991e3d3a10368275abeca0a0761720d6d68e
denzelsilvestre <denzelsilvestre@DENZELSILVE132D>
2025-04-14 19:40:22 -0500

    Add project files.

---
+++ Review2.2/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        Console.WriteLine("Enter the length of the rectangle:");
+        double length = Convert.ToDouble(Console.ReadLine());//lenght
+
+        Console.WriteLine("Enter the width of the rectangle:");//width
+        double width = Convert.ToDouble(Console.ReadLine());
+
+
+        double area =  length * width;//area equation
+
+        double perimeter = length + width * 2;//perimeter equation
+
+        Console.WriteLine("Results:");
+
+        Console.WriteLine("Area of the rectangle:" + area);
+        Console.WriteLine("Perimeter of the rectangle:" + perimeter);
+
+
+
+
+    }
+}
