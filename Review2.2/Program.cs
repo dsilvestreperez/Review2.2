0789014890e6e0230bb87f296c118b21d9efc481
dsilvestreperez <dsilvestreperez@amarillocollege.com>
2025-04-21 12:36:17 -0500

    Update Program.cs

--- Review2.2/Program.cs
+++ Review2.2/Program.cs
@@ -12,21 +12,21 @@ namespace Consoledocumation
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter the length of the rectangle:");
-            double length = Convert.ToDouble(Console.ReadLine());//lenght
+           Console.WriteLine("Enter the length of the rectangle:");
+            double length = Convert.ToDouble(Console.ReadLine());//string variable for the input of length
 
-            Console.WriteLine("Enter the width of the rectangle:");//width
-            double width = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the width of the rectangle:");//string variable  for the input of witdh
+                double width = Convert.ToDouble(Console.ReadLine());
 
 
-            double area = length * width;//area equation
+                double area = length * width;
 
-            double perimeter = length + width * 2;//perimeter equation
+            double perimeter = length + width * 2;
 
             Console.WriteLine("Results:");
 
-            Console.WriteLine("Area of the rectangle:" + area);// output of area
-            Console.WriteLine("Perimeter of the rectangle:" + perimeter);//output of perimeter
+            Console.WriteLine("Area of the rectangle:" + area);//Print the value of the area
+            Console.WriteLine("Perimeter of the rectangle:" + perimeter);//Print the value of the perimeter
 
         }
 
