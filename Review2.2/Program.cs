76e949269c8ed9f0e9c32b383d2bcf719afcec81
dsilvestreperez <dsilvestreperez@amarillocollege.com>
2025-04-20 19:40:38 -0500

    Update Program.cs

--- Review2.2/Program.cs
+++ Review2.2/Program.cs
@@ -1,30 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
 
-class Program
+namespace Consoledocumation
 {
-    static void Main(string[] args)
+    internal class Program
     {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the length of the rectangle:");
+            double length = Convert.ToDouble(Console.ReadLine());//lenght
+
+            Console.WriteLine("Enter the width of the rectangle:");//width
+            double width = Convert.ToDouble(Console.ReadLine());
 
 
-        Console.WriteLine("Enter the length of the rectangle:");
-        double length = Convert.ToDouble(Console.ReadLine());//lenght
+            double area = length * width;//area equation
 
-        Console.WriteLine("Enter the width of the rectangle:");//width
-        double width = Convert.ToDouble(Console.ReadLine());
+            double perimeter = length + width * 2;//perimeter equation
 
+            Console.WriteLine("Results:");
 
-        double area =  length * width;//area equation
+            Console.WriteLine("Area of the rectangle:" + area);// output of area
+            Console.WriteLine("Perimeter of the rectangle:" + perimeter);//output of perimeter
 
-        double perimeter = length + width * 2;//perimeter equation
-
-        Console.WriteLine("Results:");
-
-        Console.WriteLine("Area of the rectangle:" + area);
-        Console.WriteLine("Perimeter of the rectangle:" + perimeter);
-
-
-
+        }
 
     }
+
 }
