686595d01e4ee98a8c00f30cdbf7b0678a395558
dsilvestreperez <dsilvestreperez@amarillocollege.com>
2025-04-27 21:25:30 -0500

    Update Program.cs

--- Review2.2/Program.cs
+++ Review2.2/Program.cs
@@ -12,21 +12,21 @@ namespace Consoledocumation
         static void Main(string[] args)
         {
 
-           Console.WriteLine("Enter the length of the rectangle:");
-            double length = Convert.ToDouble(Console.ReadLine());//string variable for the input of length
+           Console.WriteLine("Enter the length of the rectangle:");//entering the length of the two side of the rectangle
+            double length = Convert.ToDouble(Console.ReadLine());//input of the lenght for both side of the rectangle
 
-            Console.WriteLine("Enter the width of the rectangle:");//string variable  for the input of witdh
-                double width = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the width of the rectangle:");//entering the width of the two side
+                double width = Convert.ToDouble(Console.ReadLine());//input of the width of the two side of the rectangle 
 
 
-                double area = length * width;
+                double area = length * width;//equation for the basic area
 
-            double perimeter = length + width * 2;
+            double perimeter = length + width * 2;//the equation for the basic perimeter
 
             Console.WriteLine("Results:");
 
-            Console.WriteLine("Area of the rectangle:" + area);//Print the value of the area
-            Console.WriteLine("Perimeter of the rectangle:" + perimeter);//Print the value of the perimeter
+            Console.WriteLine("Area of the rectangle:" + area);//display the total area of the rectangle
+            Console.WriteLine("Perimeter of the rectangle:" + perimeter);//display the total perimeter of the rectangle
 
         }
 
